6c39a05175d940c2b68968b7c3f88b3a2f0568fa
Patrice Waechter-Ebling <patdev.qc.ca@outlook.com>
2025-02-11 14:27:14 -0500

    Modification module gestion SEL

--- Migrations/20250211192617_Reformatage.Designer.cs
+++ Migrations/20250211192617_Reformatage.Designer.cs
@@ -4,6 +4,7 @@ using GestionEntreprise.DataBase;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GestionEntreprise.Migrations
 {
     [DbContext(typeof(GestionEntrepriseContext))]
-    partial class GestionEntrepriseContextModelSnapshot : ModelSnapshot
+    [Migration("20250211192617_Reformatage")]
+    partial class Reformatage
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250211192617_Reformatage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionEntreprise.Migrations
+{
+    /// <inheritdoc />
+    public partial class Reformatage : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
